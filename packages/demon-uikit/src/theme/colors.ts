db318312a83f42981bf0310224c2c88d7a2b661d
0xmysEE <0xmysee@protonmail.com>
2021-12-22 16:55:55 -0300

    removed demonbutton

--- packages/demon-uikit/src/theme/colors.ts
+++ packages/demon-uikit/src/theme/colors.ts
@@ -1,13 +1,13 @@
 import { Colors } from "./types";
 
 export const baseColors = {
-  failure: "#ED4B9E",
-  primary: "#1FC7D4",
-  primaryBright: "#53DEE9",
-  primaryDark: "#0098A1",
-  secondary: "#7645D9",
-  success: "#31D0AA",
-  warning: "#FFB237",
+  failure: "#FF6F66",
+  primary: "#FF6F66",
+  primaryBright: "#FF6F66",
+  primaryDark: "#FF6F66",
+  secondary: "#FF6F66",
+  success: "#66FFB6",
+  warning: "#FFCC48",
 };
 
 export const additionalColors = {
